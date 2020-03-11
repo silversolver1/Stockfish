2229d3f48f24a66a2e212cec080baea09bc1ad21
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-03-11 14:37:34 -0500

    playfor_2z

--- src/search.cpp
+++ src/search.cpp
@@ -1627,8 +1627,8 @@ moves_loop: // When in check, search starts from here
     PieceType captured = type_of(pos.piece_on(to_sq(bestMove)));
 
     bonus1 = stat_bonus(depth + 1);
-    bonus2 = bestValue > beta + PawnValueMg ? bonus1               // larger bonus
-                                            : stat_bonus(depth);   // smaller bonus
+    bonus2 = bestValue > beta + BishopValueMg ? bonus1               // larger bonus
+                                              : 0;                   // no bonus
 
     if (!pos.capture_or_promotion(bestMove))
     {
