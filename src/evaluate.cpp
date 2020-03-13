a027b3cafcfd96b8437d53af47e511fd1038f902
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-03-13 10:36:01 -0500

    5089875

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -291,7 +291,7 @@ namespace {
         if (Pt == BISHOP || Pt == KNIGHT)
         {
             // Bonus if piece is on an outpost square or can reach one
-            bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
+            bb = OutpostRanks & attackedBy[Us][PAWN];
             if (bb & s)
                 score += Outpost * (Pt == KNIGHT ? 2 : 1);
 
