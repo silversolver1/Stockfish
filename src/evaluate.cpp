d20f85003462e92cdd0e0ddef5db08d4d8eedb70
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-03-13 12:24:25 -0500

    5260584

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -540,9 +540,6 @@ namespace {
     b  = shift<Up>(pos.pieces(Us, PAWN)) & ~pos.pieces();
     b |= shift<Up>(b & TRank3BB) & ~pos.pieces();
 
-    // Keep only the squares which are relatively safe
-    b &= ~attackedBy[Them][PAWN] & safe;
-
     // Bonus for safe pawn threats on the next move
     b = pawn_attacks_bb<Us>(b) & nonPawnEnemies;
     score += ThreatByPawnPush * popcount(b);
