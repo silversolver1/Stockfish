70539e230b458d7a985c88f7691de03329a62e5b
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-03-14 16:43:12 -0500

    protection with king and/or queen is not strong protection. Bench: 5601481

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -491,8 +491,9 @@ namespace {
 
     // Squares strongly protected by the enemy, either because they defend the
     // square with a pawn, or because they defend the square twice and we don't.
-    stronglyProtected =  attackedBy[Them][PAWN]
-                       | (attackedBy2[Them] & ~attackedBy2[Us]);
+    stronglyProtected =  attackedBy[Them][PAWN] | (attackedBy2[Them] &
+                         ~attackedBy2[Us] & ~attackedBy[Them][QUEEN] &
+                         ~attackedBy[Them][KING]);
 
     // Non-pawn enemies, strongly protected
     defended = nonPawnEnemies & stronglyProtected;
