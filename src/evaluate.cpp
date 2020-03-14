4a558e346c654b2e0b67072cfa583c9ee28f88fc
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-03-14 15:28:20 -0500

    4836389

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -498,7 +498,7 @@ namespace {
     defended = nonPawnEnemies & stronglyProtected;
 
     // Enemies not strongly protected and under our attack
-    weak = pos.pieces(Them) & ~stronglyProtected & attackedBy[Us][ALL_PIECES];
+    weak = pos.pieces(Them) & ~stronglyProtected & attackedBy[Us][ALL_PIECES] & ~attackedBy[Us][KING];
 
     // Bonus according to the kind of attacking pieces
     if (defended | weak)
