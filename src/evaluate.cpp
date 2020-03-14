f437ca9110b1e41a070e699012a3dba844e4fe55
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-03-14 16:26:59 -0500

    avoid protecting with king. not strong protection. Bench: 5323161

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -491,8 +491,8 @@ namespace {
 
     // Squares strongly protected by the enemy, either because they defend the
     // square with a pawn, or because they defend the square twice and we don't.
-    stronglyProtected =  attackedBy[Them][PAWN]
-                       | (attackedBy2[Them] & ~attackedBy2[Us]);
+    stronglyProtected =  attackedBy[Them][PAWN] | (attackedBy2[Them] &
+                        ~attackedBy2[Us] & ~attackedBy[Them][KING]);
 
     // Non-pawn enemies, strongly protected
     defended = nonPawnEnemies & stronglyProtected;
