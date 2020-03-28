dd9fdbdc924a9a074d8a359cba81b46d7ea68df3
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-03-27 21:27:42 -0500

    add condition to check extension to make it more restrictive. Bench: 5278133

--- src/search.cpp
+++ src/search.cpp
@@ -1076,7 +1076,7 @@ moves_loop: // When in check, search starts from here
       }
 
       // Check extension (~2 Elo)
-      else if (    givesCheck
+      else if (    givesCheck && depth <= 10
                && (pos.is_discovery_check_on_king(~us, move) || pos.see_ge(move)))
           extension = 1;
 
