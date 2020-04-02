ff8c7e292724709abb05ba760afbcc8cf0e97308
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-04-02 11:24:19 -0500

    add different cond to futility pruning. Bench: 5259430

--- src/search.cpp
+++ src/search.cpp
@@ -837,6 +837,7 @@ namespace {
     // Step 8. Futility pruning: child node (~50 Elo)
     if (   !PvNode
         &&  depth < 6
+        &&  eval <= BishopValueEg
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
