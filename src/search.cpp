b110a8293b9760a40f45d20af272ffff2ef424fa
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-04-18 12:12:00 -0500

    add cond to capture history based pruning. Bench: 4547354

--- src/search.cpp
+++ src/search.cpp
@@ -1045,7 +1045,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-                  && lmrDepth < 1
+                  && lmrDepth < 1 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
