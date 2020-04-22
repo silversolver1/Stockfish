eaa652be521ed21c4d5bfa419ebdbfba4f73d1e7
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-04-22 13:18:04 -0500

    sort all moves, not just those greater than or equal to limit. Bench: 4587347

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -31,19 +31,17 @@ namespace {
     QSEARCH_TT, QCAPTURE_INIT, QCAPTURE, QCHECK_INIT, QCHECK
   };
 
-  // partial_insertion_sort() sorts moves in descending order up to and including
-  // a given limit. The order of moves smaller than the limit is left unspecified.
-  void partial_insertion_sort(ExtMove* begin, ExtMove* end, int limit) {
+  // partial_insertion_sort() sorts moves in descending order.
+  void partial_insertion_sort(ExtMove* begin, ExtMove* end) {
 
     for (ExtMove *sortedEnd = begin, *p = begin + 1; p < end; ++p)
-        if (p->value >= limit)
-        {
+    {
             ExtMove tmp = *p, *q;
             *p = *++sortedEnd;
             for (q = sortedEnd; q != begin && *(q - 1) < tmp; --q)
                 *q = *(q - 1);
             *q = tmp;
-        }
+    }
   }
 
 } // namespace
@@ -201,7 +199,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves);
       }
 
       ++stage;
