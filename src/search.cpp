aa6477fc82a065a5367c79c1faca2e9e9a0476d1
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-04-25 11:44:09 -0500

    further increase reduction for cut nodes. Bench: 4254245

--- src/search.cpp
+++ src/search.cpp
@@ -1198,7 +1198,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2;
+                  r += 3;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
