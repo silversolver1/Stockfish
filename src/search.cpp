d2b0c4f437877d8f48b4bf8aa450ce0a8c421348
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-05-01 15:37:54 -0500

    don't restrict increasing reduction for cut nodes to only !captureOrPromotion. include captures or promotions as well. might also try with r++. Bench: 4372437

--- src/search.cpp
+++ src/search.cpp
@@ -1197,15 +1197,15 @@ moves_loop: // When in check, search starts from here
           if (singularLMR)
               r -= 1 + formerPv;
 
+          // Increase reduction for cut nodes (~10 Elo)
+          if (cutNode)
+              r += 2;
+
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
                   r++;
-
-              // Increase reduction for cut nodes (~10 Elo)
-              if (cutNode)
-                  r += 2;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
