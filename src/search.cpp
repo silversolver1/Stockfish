1f8f27c736f91398645ef6f2e4c713674c86516f
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-05-01 12:34:33 -0500

    change see based pruning to be directly -PawnValuEg dependent (-213 vs -194). Bench: 4972142

--- src/search.cpp
+++ src/search.cpp
@@ -1057,7 +1057,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // See based pruning
-              if (!pos.see_ge(move, Value(-194) * depth)) // (~25 Elo)
+              if (!pos.see_ge(move, Value(-PawnValueEg) * depth)) // (~25 Elo)
                   continue;
           }
       }
