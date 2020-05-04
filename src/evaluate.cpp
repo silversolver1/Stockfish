7d7b424146dc802d5ccabd5eda8f6cb18dddc896
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-05-04 18:28:05 -0500

    try and bring blocked_count into initiative. might try different values. Bench: 4593562

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -734,6 +734,7 @@ namespace {
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
+                    +  8 * pe->blocked_count()
                     - 43 * almostUnwinnable
                     -110 ;
 
