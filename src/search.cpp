fce85e005854a874908eb94cc82d598764e18c96
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-05-27 12:15:31 -0500

    try removing one cond from nmp. Bench: 4635418

--- src/search.cpp
+++ src/search.cpp
@@ -850,7 +850,6 @@ namespace {
     // Step 9. Null move search with verification search (~40 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && (ss-1)->statScore < 23397
         &&  eval >= beta
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 32 * depth - 30 * improving + 120 * ttPv + 292
