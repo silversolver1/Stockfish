c8d2c9039365c981c93b1787f20988a85ad8cd87
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-06-01 11:14:40 -0500

    add condition to passed pawn extension. Bench: 4428290

--- src/search.cpp
+++ src/search.cpp
@@ -1123,6 +1123,7 @@ moves_loop: // When in check, search starts from here
 
       // Passed pawn extension
       else if (   move == ss->killers[0]
+               && (ss-1)->currentMove != (ss-1)->killers[0]
                && pos.advanced_pawn_push(move)
                && pos.pawn_passed(us, to_sq(move)))
           extension = 1;
