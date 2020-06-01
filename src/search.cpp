4221e1eb7ec28442552a7e3cb122c296e7df79d0
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-06-01 11:20:57 -0500

    add cond to make it harder to give extra bonus to moves in step 17. Bench: 4909578

--- src/search.cpp
+++ src/search.cpp
@@ -1271,7 +1271,7 @@ moves_loop: // When in check, search starts from here
               int bonus = value > alpha ?  stat_bonus(newDepth)
                                         : -stat_bonus(newDepth);
 
-              if (move == ss->killers[0])
+              if (move == ss->killers[0] && (ss-1)->currentMove != (ss-1)->killers[0])
                   bonus += bonus / 4;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
