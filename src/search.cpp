16de60ffcdf551f05127831be7e191ba4a8eac15
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-06-13 15:03:53 -0500

    conditionally increase late irreversible move extension. Bench: 4153123

--- src/search.cpp
+++ src/search.cpp
@@ -1110,7 +1110,7 @@ moves_loop: // When in check, search starts from here
       if (   move == ttMove
           && pos.rule50_count() > 80
           && (captureOrPromotion || type_of(movedPiece) == PAWN))
-          extension = 2;
+          extension = 2 + (abs(ttValue) < 2);
 
       // Add extension to new depth
       newDepth += extension;
