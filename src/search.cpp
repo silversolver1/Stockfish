3c6b8ab536de9b68902e2b1c9dea271f4ce53c60
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-06-23 12:04:24 -0500

    try to extend more when enemy pieces are not on the same file as the square our passed pawn is moving to. Bench: 4789004

--- src/search.cpp
+++ src/search.cpp
@@ -1095,7 +1095,7 @@ moves_loop: // When in check, search starts from here
       else if (   move == ss->killers[0]
                && pos.advanced_pawn_push(move)
                && pos.pawn_passed(us, to_sq(move)))
-          extension = 1;
+          extension = 1 + !(file_bb(to_sq(move)) & pos.pieces(~us));
 
       // Last captures extension
       else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
