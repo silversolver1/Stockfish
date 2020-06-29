7afbe9a378f591fddd6d47eade748c4f2ae785f5
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-06-28 23:19:13 -0500

    add condition to step 13. Bench: 4541748

--- src/search.cpp
+++ src/search.cpp
@@ -984,7 +984,8 @@ moves_loop: // When in check, search starts from here
       // Step 13. Pruning at shallow depth (~200 Elo)
       if (  !rootNode
           && pos.non_pawn_material(us)
-          && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
+          && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
+          && bestValue < VALUE_KNOWN_WIN)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
