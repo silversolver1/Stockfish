3af3f3cd83364805bb0de85de6646cd0c6808c94
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-07-28 10:26:40 -0500

    try adding !extension condition to LMR (step 16). Bench: 4265705

--- src/search.cpp
+++ src/search.cpp
@@ -1169,6 +1169,7 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
+              || !extension
               || thisThread->ttHitAverage < 415 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
